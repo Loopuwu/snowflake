d18afd3fab9eb350b71fb191470d3c4eb305217c
Loopuwu <97133301+Loopuwu@users.noreply.github.com>
2024-05-16 17:38:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 
 # Write directly to the app
-#st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
+st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie !
     """
@@ -18,12 +18,12 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 #session = get_active_session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 #convert dataframe to panda object
-#pd_df = my_dataframe.to_pandas()
+pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
@@ -41,8 +41,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen
         
-        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         
         st.subheader(fruit_chosen + 'Nutrition information')
